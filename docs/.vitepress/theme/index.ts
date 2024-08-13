ba13906e4f13a630810733a211545d7f65341601
mcgdg <https://github.com/SunnyYie/>
2024-08-13 22:56:57 +0800

    feature:新增unocss，修改链接样式

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,34 +3,16 @@ import type { EnhanceAppContext, Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 import { nextTick, onMounted, watch } from 'vue';
 import mediumZoom from 'medium-zoom';
-import { registerAnalytics, siteIds, trackPageview } from './plugins/baidutongji';
-import googleAnalytics from './plugins/googleAnalytics';
 import './styles/main.css';
-import './styles/global.css'
-import './styles/demo.css'
-import './styles/utils.css'
-import './styles/vars.css'
-// import 'uno.css'
+import './styles/global.css';
+import './styles/demo.css';
+import './styles/utils.css';
+import './styles/vars.css';
+import 'uno.css';
 
 const theme: Theme = {
   ...DefaultTheme,
-  enhanceApp({ router }: EnhanceAppContext) {
-    googleAnalytics({
-      id: 'G-0F3DLK5BSG',
-    })
-    if (inBrowser) {
-      registerAnalytics(siteIds)
 
-      window.addEventListener('hashchange', () => {
-        const { href: url } = window.location
-        trackPageview(siteIds, url)
-      })
-
-      router.onAfterRouteChanged = (to) => {
-        trackPageview(siteIds, to)
-      }
-    }
-  },
   setup() {
     const route = useRoute()
     const initZoom = () => {
